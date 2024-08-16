259674e2c55d925033a7fd9bc00b3bb92eda4e3b
rem <86702826+reyyymartt@users.noreply.github.com>
2024-08-16 20:05:12 +0800

    Add files via upload

---
+++ modules/module.js
@@ -0,0 +1,26 @@
+
+
+
+const config = {
+  version:1,
+  owner:"reymart",
+  title:"Profile",
+  profile_info: {
+    Name: "Reymart Centeno"
+  }
+}
+
+class Student{
+  constructor(name, age){
+    this.name = name;
+    this.age = age;
+    this.status = "alive"
+    this.kill = function (){
+      this.status = "dead"
+    }
+  }
+}
+
+const Earl = new Student("Earl Matthew", 18)
+
+export default config
